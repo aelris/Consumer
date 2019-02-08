63470d612a14864bdb40e0e93c2b4e89064d4ef4
aelris <aelrismira@gmail.com>
2019-02-08 22:50:27 +0200

    add hdfs config path

--- src/main/java/com/epam/streaming/SparkJob.scala
+++ src/main/java/com/epam/streaming/SparkJob.scala
@@ -1,28 +1,31 @@
 package com.epam.streaming
 
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.LocalFileSystem
 import org.apache.hadoop.hdfs.DistributedFileSystem
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 
 object SparkJob {
-  private var csvPath = "hdfs://sandbox-hdp.hortonworks.com:8020/homework/streaming/streaming.csv"
+  private var csvPath = "hdfs://sandbox-hdp.hortonworks.com:8020/homework/streaming"
 
   def sparkJob() {
 
     val spark: SparkSession = SparkSession
       .builder()
       .master("local")
-      .config("fs.hdfs.impl", classOf[DistributedFileSystem].getName)
-      .config("fs.file.impl", classOf[LocalFileSystem].getName)
       .getOrCreate()
+    import org.apache.hadoop.fs.FileSystem
+    import java.net.URI
+    val fsConf =new Configuration()
+    fsConf.set("fs.hdfs.impl", classOf[DistributedFileSystem].getName)
+    fsConf.set("fs.file.impl", classOf[LocalFileSystem].getName)
+    FileSystem.get(URI.create(csvPath), fsConf)
 
     val dataFrameKafkaRecords: DataFrame = spark
       .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
-      .option("fs.hdfs.impl", classOf[DistributedFileSystem].getName)
-      .option("fs.file.impl", classOf[LocalFileSystem].getName)
       .option("subscribe", "StreamingTopic")
       .csv(csvPath)
 
